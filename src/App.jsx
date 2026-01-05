f1bacff6d3dc4ad0800e5aa95314ea8c3c8c3383
williams bolu <williams.bolu99@gmail.com>
2026-01-05 19:48:43 +0100

    implement the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import gsap from "gsap";
-import { ScrollToPlugin, SplitText } from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 // We register the pluging within the app because this gsap plugins aren't automatically active we have to register them.
 // This line make sure that both plugins are ready to use globally across the app
-gsap.registerPlugin(ScrollToPlugin, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-8xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 };
 
